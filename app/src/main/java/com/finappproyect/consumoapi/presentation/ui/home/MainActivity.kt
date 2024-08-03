9f4cb828a6380928974f9f8569226185dbc22a48
LucasBarrasa <lucasbarrasa96@gmail.com>
2024-08-03 03:05:08 -0300

    Initial commit

---
+++ app/src/main/java/com/finappproyect/consumoapi/presentation/ui/home/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.finappproyect.consumoapi.presentation.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.finappproyect.consumoapi.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+}
